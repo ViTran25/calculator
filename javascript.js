1c264416863d835885ec780f9c0ca529326e3102
Vi Tran <77949259+ViTran25@users.noreply.github.com>
2023-01-05 23:29:11 -0500

    Create 'operate()' function
    The function takes 2 numbers and an operator and calls the corresponding
    function based on the operator given.

--- javascript.js
+++ javascript.js
@@ -6,3 +6,22 @@ const subtract = (a, b) => a-b;
 const multiply = (a, b) => a*b;
 
 const divide = (a, b) => a/b;
+//
+function operate(a, b, operator) {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case 'x':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
